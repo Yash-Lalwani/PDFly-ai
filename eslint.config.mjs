1fc55521459e015a08af2b7411920de6df97abf2
Yash Lalwani <yash@macbookair.mynetworksettings.com>
2025-08-19 02:09:55 -0400

    Project & Shadcn Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = {
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+      "plugin:tailwindcss/recommended",
+    ],
+    plugins: ["prettier"], // Lets ESLint run Prettier and report formatting errors.
+    rules: {
+      "prettier/prettier": "error", // If Prettier finds formatting issues, ESLint marks them as errors.
+      "react/no-escape-entities": "off", // Turns off the rule that complains about things like using ’ or ” directly in JSX.
+    },
+  }),
+};
 
 export default eslintConfig;
