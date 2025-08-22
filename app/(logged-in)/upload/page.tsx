c85e5ac7352f0f3536e389db0113fda607f6e6bc
Yash Lalwani <yash@Yashs-MacBook-Air.local>
2025-08-21 20:51:42 -0400

    added upload page

---
+++ app/(logged-in)/upload/page.tsx
@@ -0,0 +1,15 @@
+import BgGradient from "@/components/common/bg-gradient";
+import { FileUploadDemo } from "@/components/upload/upload-form";
+import UploadHeader from "@/components/upload/upload-header";
+
+export default function Page() {
+  return (
+    <section className="min-h-screen">
+      <BgGradient />
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+        <UploadHeader />
+        <FileUploadDemo />
+      </div>
+    </section>
+  );
+}
